1659e1d70403ad42840c5607a4447b37edd5447a
Yifan RIGNY <yrigny@student.42.fr>
2025-04-19 23:50:41 +0200

    added input check for register page

--- public/displayRegister.js
+++ public/displayRegister.js
@@ -1,4 +1,3 @@
-
 function displayRegister() {
 	fetch('register.html')
 		.then(response => {
@@ -10,8 +9,39 @@ function displayRegister() {
 		.then(html => {
 			const targetDiv = document.getElementById('register-inject');
 			targetDiv.innerHTML = html;
+			checkInputFields();
 		})
 		.catch(err => console.error('Failed to fetch register.html:', err));
+}
+
+function checkInputFields() {
+	const usernameInput = document.getElementById('username');
+	const passwordInput = document.getElementById('password');
+	const confirmPasswordInput = document.getElementById('confirm-password');
+	const errorMessage = document.getElementById('error-message');
+
+	usernameInput.addEventListener('blur', () => {
+		console.log('Username input:', usernameInput.value);
+		if (usernameInput.value.length < 3) {
+			errorMessage.textContent = 'Username must be at least 3 characters long.';
+		} else {
+			errorMessage.textContent = '';
+		}
+	});
+	passwordInput.addEventListener('blur', () => {
+		if (passwordInput.value.length < 6) {
+			errorMessage.textContent = 'Password must be at least 6 characters long.';
+		} else {
+			errorMessage.textContent = '';
+		}
+	});
+	confirmPasswordInput.addEventListener('blur', () => {
+		if (passwordInput.value !== confirmPasswordInput.value) {
+			errorMessage.textContent = 'Passwords do not match.';
+		} else {
+			errorMessage.textContent = '';
+		}
+	})
 }
 
 export default displayRegister;
